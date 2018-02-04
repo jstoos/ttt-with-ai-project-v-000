df6808f331f34cabea933f8f5ec3fcaeb7e1c0dd
Jennifer Stoos <jstoos@bitlaw.com>
2018-02-04 17:11:57 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,23 +4,23 @@ module Players
     def move(board)
       #Top row, prevent win
 
-       if board.cells[0] != self.token && board.cells[0] != " " && board.cells[1] != self.token && board.cells[1] != " "
+       if board.cells[0] != self.token && board.cells[0] != " " && board.cells[1] != self.token && board.cells[1] != " " && board.valid_move?("3")
          puts "#{self.token} plays in space 3"
            "3"
-       elsif board.cells[1] != self.token && board.cells[1] != " " && board.cells[2] != self.token && board.cells[2] != " "
+       elsif board.cells[1] != self.token && board.cells[1] != " " && board.cells[2] != self.token && board.cells[2] != " " && board.valid_move?("1")
          puts "#{self.token} plays in space 1"
            "1"
-       elsif board.cells[0] != self.token && board.cells[0] != " " && board.cells[2] != self.token && board.cells[2] != " "
+       elsif board.cells[0] != self.token && board.cells[0] != " " && board.cells[2] != self.token && board.cells[2] != " " && board.valid_move?("2")
          puts "#{self.token} plays in space 2"
            "2"
      #Middle row, prevent win
-       elsif board.cells[3] != self.token && board.cells[3] != " " && board.cells[4] != self.token && board.cells[4] != " "
+   elsif board.cells[3] != self.token && board.cells[3] != " " && board.cells[4] != self.token && board.cells[4] != " " && board.valid_move?("6")
          puts "#{self.token} plays in space 6"
            "6"
-       elsif board.cells[4] != self.token && board.cells[4] != " " && board.cells[5] != self.token && board.cells[5] != " "
+       elsif board.cells[4] != self.token && board.cells[4] != " " && board.cells[5] != self.token && board.cells[5] != " " && board.valid_move?("4")
          puts "#{self.token} plays in space 4"
            "4"
-       elsif board.cells[3] != self.token && board.cells[3] != " " && board.cells[5] != self.token && board.cells[5] != " "
+       elsif board.cells[3] != self.token && board.cells[3] != " " && board.cells[5] != self.token && board.cells[5] != " " && board.valid_move?("5")
           puts "#{self.token} plays in space 5"
             "5"
       #Bottom row, prevent win
@@ -37,17 +37,10 @@ module Players
         elsif board.cells[0] != self.token && board.cells[0] != " " && board.cells[4] != self.token && board.cells[4] != " " && board.valid_move?("9")
             puts "#{self.token} plays in space 9"
             "9"
- +
         elsif board.cells[4] != self.token && board.cells[4] != " " && board.cells[8] != self.token && board.cells[8] != " " && board.valid_move?("1")
             puts "#{self.token} plays in space 1"
             "1"
-        elsif board.cells[0] != self.token && board.cells[0] != " " && board.cells[8] != self.token && board.cells[8] != " " && board.valid_move?("5")
- +
- +      elsif board.cells[4] != self.token && board.cells[4] != " " && board.cells[8] != self.token && board.cells[8] != " "
- +          puts "#{self.token} plays in space 1"
- +          "1"
- +      elsif board.cells[0] != self.token && board.cells[0] != " " && board.cells[8] != self.token && board.cells[8] != " "
- +
+       elsif board.cells[0] != self.token && board.cells[0] != " " && board.cells[8] != self.token && board.cells[8] != " " && board.valid_move?("5")
             puts "#{self.token} plays in space 5"
             "5"
       #Kitty corner left to right, prevent win
@@ -146,16 +139,16 @@ module Players
        else
        end
      end
- 
+
    end
- 
+
  end
 
 
 
 #     def move(board)
 #     #Top row, prevent win
-# 
+#
 #       if board.cells[0] != self.token && board.cells[0] != " " && board.cells[1] != self.token && board.cells[1] != " " && board.valid_move?("3")
 #           puts "#{self.token} plays in space 3"
 #             "3"
@@ -214,51 +207,51 @@ module Players
 #       elsif board.cells[0] != self.token && board.cells[0] != " " && board.cells[4] != self.token && board.cells[4] != " " && board.valid_move?("9")
 #           puts "#{self.token} plays in space 9"
 #           "9"
-# 
+#
 #       elsif board.cells[4] != self.token && board.cells[4] != " " && board.cells[8] != self.token && board.cells[8] != " " && board.valid_move?("1")
 #           puts "#{self.token} plays in space 1"
 #           "1"
 #       elsif board.cells[0] != self.token && board.cells[0] != " " && board.cells[8] != self.token && board.cells[8] != " " && board.valid_move?("5")
-# 
+#
 #       elsif board.cells[4] != self.token && board.cells[4] != " " && board.cells[8] != self.token && board.cells[8] != " "
 #           puts "#{self.token} plays in space 1"
 #           "1"
 #       elsif board.cells[0] != self.token && board.cells[0] != " " && board.cells[8] != self.token && board.cells[8] != " "
-# 
+#
 #           puts "#{self.token} plays in space 5"
 #           "5"
 #     #Kitty corner left to right, prevent win
 #       elsif board.cells[2] != self.token && board.cells[2] != " " && board.cells[4] != self.token && board.cells[4] != " " && board.valid_move?("7")
 #           puts "#{self.token} plays in space 7"
 #           "7"
-# 
+#
 #       elsif board.cells[4] != self.token && board.cells[4] != " " && board.cells[6] != self.token && board.cells[6] != " " && board.valid_move?("3")
 #           puts "#{self.token} plays in space 3"
 #           "3"
 #       elsif board.cells[2] != self.token && board.cells[2] != " " && board.cells[6] != self.token && board.cells[6] != " " && board.valid_move?("5")
-# 
+#
 #       elsif board.cells[4] != self.token && board.cells[4] != " " && board.cells[6] != self.token && board.cells[6] != " "
 #           puts "#{self.token} plays in space 3"
 #           "3"
 #       elsif board.cells[2] != self.token && board.cells[2] != " " && board.cells[6] != self.token && board.cells[6] != " "
-# 
+#
 #           puts "#{self.token} plays in space 5"
 #           "5"
 #       #Left column, prevent win
 #       elsif board.cells[0] != self.token && board.cells[0] != " " && board.cells[3] != self.token && board.cells[3] != " " && board.valid_move?("7")
 #           puts "#{self.token} plays in space 7"
 #           "7"
-# 
+#
 #       elsif board.cells[3] != self.token && board.cells[3] != " " && board.cells[6] != self.token && board.cells[6] != " " && board.valid_move?("1")
 #           puts "#{self.token} plays in space 1"
 #           "1"
 #       elsif board.cells[0] != self.token && board.cells[0] != " " && board.cells[6] != self.token && board.cells[6] != " " && board.valid_move?("4")
-# 
+#
 #       elsif board.cells[3] != self.token && board.cells[3] != " " && board.cells[6] != self.token && board.cells[6] != " "
 #           puts "#{self.token} plays in space 1"
 #           "1"
 #       elsif board.cells[0] != self.token && board.cells[0] != " " && board.cells[6] != self.token && board.cells[6] != " "
-# 
+#
 #           puts "#{self.token} plays in space 4"
 #           "4"
 #       #Middle column, prevent win
@@ -269,19 +262,19 @@ module Players
 #           puts "#{self.token} plays in space 2"
 #           "2"
 #       elsif board.cells[1] != self.token && board.cells[1] != " " && board.cells[7] != self.token && board.cells[7] != " " && board.valid_move?("5")
-# 
+#
 #       elsif board.cells[4] != self.token && board.cells[4] != " " && board.cells[7] != self.token && board.cells[7] != " "
 #           puts "#{self.token} plays in space 2"
 #           "2"
 #       elsif board.cells[1] != self.token && board.cells[1] != " " && board.cells[7] != self.token && board.cells[7] != " "
-# 
+#
 #           puts "#{self.token} plays in space 5"
 #           "5"
 #       #Right column, prevent win
 #       elsif board.cells[2] != self.token && board.cells[2] != " " && board.cells[5] != self.token && board.cells[5] != " " && board.valid_move?("9")
 #           puts "#{self.token} plays in space 9"
 #           "9"
-# 
+#
 #       elsif board.cells[5] != self.token && board.cells[5] != " " && board.cells[8] != self.token && board.cells[8] != " " && board.valid_move?("3")
 #           puts "#{self.token} plays in space 3"
 #           "3"
@@ -289,7 +282,7 @@ module Players
 #           puts "#{self.token} plays in space 6"
 #           "6"
 #   #There are no defensive moves to make
-# 
+#
 #       elsif board.cells[5] != self.token && board.cells[5] != " " && board.cells[8] != self.token && board.cells[8] != " "
 #           puts "#{self.token} plays in space 3"
 #           "3"
@@ -326,9 +319,9 @@ module Players
 #       else
 #       end
 #     end
-# 
+#
 #   end
-# 
+#
 # end
 
 
